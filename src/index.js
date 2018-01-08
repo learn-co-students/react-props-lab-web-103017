11bb5e9eaeefb62b6bfecf01c4b32a512cdc154b
erickalexander <erickcamacho1@gmail.com>
2018-01-08 16:05:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import Spaceship from './components/Spaceship';
 
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <Spaceship name="Millennium Falcon"
+    speed= {50}
+    hasRockets= {true}
+    colors = {['green','black']} />,
   document.getElementById('global')
 );
